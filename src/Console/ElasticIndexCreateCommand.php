861c7f7bdd5161e222a01ed625ed155a6832f172
PerewerzewPasha <perewerzew.pavel@projektcs.com>
2018-08-31 10:56:56 +0300

    fix command for create index

--- src/Console/ElasticIndexCreateCommand.php
+++ src/Console/ElasticIndexCreateCommand.php
@@ -8,6 +8,7 @@ use ScoutElastic\Console\Features\RequiresIndexConfiguratorArgument;
 use ScoutElastic\Facades\ElasticClient;
 use ScoutElastic\Migratable;
 use ScoutElastic\Payloads\IndexPayload;
+use ScoutElastic\Payloads\RawPayload;
 
 class ElasticIndexCreateCommand extends Command
 {
@@ -27,51 +28,53 @@ class ElasticIndexCreateCommand extends Command
     {
         $configurator = $this->getIndexConfigurator();
 
+        if (! empty($configurator->getUsedModel())) {
 
-        if(!empty($configurator->getUsedModel())){
-
-            foreach ($configurator->getUsedModel() as $model){
+            foreach ($configurator->getUsedModel() as $model) {
                 $configurator->setName(call_user_func([(new $model()), 'getIndicesName']));
+                if (! $this->isExists($configurator->getName())) {
+                    $payload = (new IndexPayload($configurator))->setIfNotEmpty('body.settings', $configurator->getSettings())->setIfNotEmpty('body.mappings._default_', $configurator->getDefaultMapping())->get();
 
-                $payload = (new IndexPayload($configurator))
-                    ->setIfNotEmpty('body.settings', $configurator->getSettings())
-                    ->setIfNotEmpty('body.mappings._default_', $configurator->getDefaultMapping())
-                    ->get();
+                    ElasticClient::indices()->create($payload);
+
+                    $this->info(sprintf('The %s index was created!', $configurator->getName()));
+                } else {
+                    $this->info(sprintf('The %s index already exists!', $configurator->getName()));
+                }
+            }
+        } else {
+            if (! $this->isExists($configurator->getName())) {
+                $payload = (new IndexPayload($configurator))->setIfNotEmpty('body.settings', $configurator->getSettings())->setIfNotEmpty('body.mappings._default_', $configurator->getDefaultMapping())->get();
 
                 ElasticClient::indices()->create($payload);
 
                 $this->info(sprintf('The %s index was created!', $configurator->getName()));
+            } else {
+                $this->info(sprintf('The %s index already exists!', $configurator->getName()));
             }
-        } else {
-
-            $payload = (new IndexPayload($configurator))->setIfNotEmpty('body.settings', $configurator->getSettings())->setIfNotEmpty('body.mappings._default_', $configurator->getDefaultMapping())->get();
-
-            ElasticClient::indices()->create($payload);
-
-            $this->info(sprintf('The %s index was created!', $configurator->getName()));
         }
+    }
+
+    protected function isExists($name)
+    {
+        $payload = (new RawPayload())->set('index', $name)->get();
+
+        return ElasticClient::indices()->exists($payload);
     }
 
     protected function createWriteAlias()
     {
         $configurator = $this->getIndexConfigurator();
 
-        if (!in_array(Migratable::class, class_uses_recursive($configurator))) {
+        if (! in_array(Migratable::class, class_uses_recursive($configurator))) {
             return;
         }
 
-        $payload = (new IndexPayload($configurator))
-            ->set('name', $configurator->getWriteAlias())
-            ->get();
+        $payload = (new IndexPayload($configurator))->set('name', $configurator->getWriteAlias())->get();
 
-        ElasticClient::indices()
-            ->putAlias($payload);
+        ElasticClient::indices()->putAlias($payload);
 
-        $this->info(sprintf(
-            'The %s alias for the %s index was created!',
-            $configurator->getWriteAlias(),
-            $configurator->getName()
-        ));
+        $this->info(sprintf('The %s alias for the %s index was created!', $configurator->getWriteAlias(), $configurator->getName()));
     }
 
     public function handle()
